dc1c4917fba0bc4e18901e23a70a57bfe19c62d4
mmmin-sy <salutluvya@gmail.com>
2024-08-05 21:13:33 +0200

    Encoding search string

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   async function handle(formData) {
     'use server';
     const searchString = formData.get('search');
-    redirect(`/search/${searchString}`);
+    redirect(`/search/${encodeURIComponent(searchString)}`);
   }
   return (
     <div className="flex min-h-screen flex-col items-center justify-start p-24">
