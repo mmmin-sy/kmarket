76d8a694b7b864a9e0ab00a23b1d7e90ca4696a8
mmmin-sy <salutluvya@gmail.com>
2024-08-06 11:00:26 +0200

    Change search form style

--- fe/component/SearchForm/SearchForm.tsx
+++ fe/component/SearchForm/SearchForm.tsx
@@ -11,11 +11,16 @@ export default async function SearchForm (){
 	}
 	return (
 		<>
-			<Link href="/"  className="text-4xl font-['Pacifico'] text-custom-deepPink">Hello, K market!</Link>
+			<Link href="/"  className="text-4xl font-['Pacifico'] text-custom-deepPink mb-5">Hello, K market!</Link>
 
-			<form action={handle}>
-				<input type="text" name="search" className="p-1 border border-solid border-custom-gray-light rounded-l-md focus:outline-none"/>
-				<button className="py-1 px-2 border border-solid border-custom-gray-darker bg-custom-gray-darker text-custom-gray-lighter rounded-r-md hover:bg-custom-gray-darkest hover:border-custom-gray-darkest">search</button>
+			<form action={handle} className="w-full flex justify-center">
+				<input
+					type="text"
+					name="search"
+					className="w-1/2 p-1 border border-solid border-custom-gray-light rounded-l-md focus:outline-none"/>
+				<button
+					className="text-sm py-1 px-2 border border-solid border-custom-gray-darker bg-custom-gray-darker text-custom-gray-lighter rounded-r-md hover:bg-custom-gray-darkest hover:border-custom-gray-darkest">search
+				</button>
 			</form>
 		</>
 	);
