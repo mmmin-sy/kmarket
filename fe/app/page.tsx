26a381e5338a41c4d58882d4e4bbcd20bb218375
mmmin-sy <salutluvya@gmail.com>
2024-08-06 16:26:14 +0200

    clean up

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -1,15 +1,9 @@
 'use server'
 
-import { redirect } from "next/navigation";
 import SearchForm from '@/component/SearchForm/SearchForm';
 import { Suspense } from "react";
 
 export default async function Home() {
-  async function handle(formData) {
-    'use server';
-    const searchString = formData.get('search');
-    redirect(`/search/${encodeURIComponent(searchString)}`);
-  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Suspense fallback={<p>Loading......</p>}><SearchForm /></Suspense>
