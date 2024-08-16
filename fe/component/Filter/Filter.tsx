53ee6e2b5388fa05ac510527b729cb5e6e33d687
mmmin-sy <salutluvya@gmail.com>
2024-08-16 14:48:47 +0200

    Change slider button

--- fe/component/Filter/Filter.tsx
+++ fe/component/Filter/Filter.tsx
@@ -8,7 +8,6 @@ export default function Filter ({ max, min, unit }: FilterProps){
 	const sliderRef = React.useRef<HTMLInputElement>(null);
 	const lowerRef = React.useRef<HTMLInputElement>(null);
 	const upperRef = React.useRef<HTMLInputElement>(null);
-	const diff = React.useRef<HTMLInputElement>(null);
 
 	const [lowerPercentage, setLowerPer] = useState<number>(0);
 	const [upperPercentage, setUpperPer] = useState<number>(100);
@@ -19,89 +18,83 @@ export default function Filter ({ max, min, unit }: FilterProps){
 
 	const getPercentage = (current) => {
 		const sliderWidth = sliderRef.current.getBoundingClientRect().width;
-		return Math.floor((100 * current) / sliderWidth);
+		return Math.round((100 * current) / sliderWidth);
 	}
 
-	const handleMouseMoveLower = (event) => {
+	const handleLowerMouseMove = (event) => {
 		let position = event.clientX;
 		const start = 0;
 		const upperElement = upperRef.current.getBoundingClientRect().left - 16;
 
 		if (position < start) position = start;
-		if (position > upperElement) position = upperElement - 1;
+		if (position >= (upperElement - 10)) position = upperElement - 5;
 
 		const newPercentage = getPercentage(position);
-		if (lowerPercentage !== newPercentage) {
-			setLowerPer(newPercentage);
-			setLowerVal((max * newPercentage) / 100);
-		}
+
+		setLowerPer(newPercentage);
+		setLowerVal((max * newPercentage) / 100);
 	}
 
-	// 슬라이트버튼을 눌렀을때.
-	// moving 할수있는 이벤트 등록.
-	const handleMouseDownLower = (event) => {
-		console.log(event.clientX, lowerRef.current.getBoundingClientRect().left)
-		document.addEventListener('mousemove', handleMouseMoveLower);
-		document.addEventListener('mouseup', handleMouseUpLower);
+	const handleLowerMouseDown = () => {
+		document.addEventListener('mousemove', handleLowerMouseMove);
+		document.addEventListener('mouseup', handleLowerMouseUp);
 	}
 
-	const handleMouseUpLower = () => {
-		document.removeEventListener('mouseup', handleMouseUpLower);
-		document.removeEventListener('mousemove', handleMouseMoveLower);
+	const handleLowerMouseUp = () => {
+		document.removeEventListener('mouseup', handleLowerMouseUp);
+		document.removeEventListener('mousemove', handleLowerMouseMove);
 	}
 
-	const handleMouseDownUpper = (event) => {
-		document.addEventListener('mousemove', handleMouseMoveUpper);
-		document.addEventListener('mouseup', handleMouseUpUpper);
+	const handleUpperMouseDown = () => {
+		document.addEventListener('mousemove', handleUpperMouseMove);
+		document.addEventListener('mouseup', handleUpperMouseUp);
 	}
 
-	const handleMouseMoveUpper = (event) => {
+	const handleUpperMouseMove = (event) => {
 		let position = event.clientX;
 
 		const end = sliderRef.current.getBoundingClientRect().width;
-		const lowerElement = lowerRef.current.getBoundingClientRect().left + 8;
+		const lowerElement = lowerRef.current.getBoundingClientRect().left + 16;
 
-		if (position < lowerElement) position = lowerElement;
+		if (position <= (lowerElement - 10)) position = lowerElement - 5;
 		if (position > end) position = end;
 
 		const newPercentage = getPercentage(position);
-		if (upperPercentage !== newPercentage) {
-			setUpperPer(newPercentage);
-			setUpperVal((max * newPercentage) / 100);
-		}
+		setUpperPer(newPercentage);
+		setUpperVal((max * newPercentage) / 100);
 	}
 
-	const handleMouseUpUpper = () => {
-		document.removeEventListener('mouseup', handleMouseUpUpper);
-		document.removeEventListener('mousemove', handleMouseMoveUpper);
+	const handleUpperMouseUp = () => {
+		document.removeEventListener('mouseup', handleUpperMouseUp);
+		document.removeEventListener('mousemove', handleUpperMouseMove);
 	}
 
 	return (
 		<div className="relative">
-			<div className="h-[8px]">
+			<div className="h-[4px]">
 				<div ref={sliderRef} className="h-full border border-solid bg-custom-gray-lighter rounded">
 
-					<div className="w-full absolute top-0 left-0 h-[8px]">
+					<div className="w-full absolute top-0 left-0 h-[4px]">
 
 						<div className="relative">
 							<div
 								ref={lowerRef}
-								className="absolute w-[16px] h-[16px] top-[-4px] rounded-md bg-custom-deepPink cursor-pointer z-10"
-								onMouseDown={handleMouseDownLower}
-								onMouseUp={handleMouseUpLower}
-								style={{left: `calc(${lowerPercentage}% - 8px)`}}
+								className="absolute w-[16px] h-[16px] top-[-6px] rounded-lg bg-custom-deepPink cursor-pointer z-10"
+								onMouseDown={handleLowerMouseDown}
+								onMouseUp={handleLowerMouseUp}
+								style={{left: `calc(${lowerPercentage}% - 16px)`}}
 							>
 							</div>
 							<div
-								className="absolute bg-custom-deepPink h-[8px] w-full z-0"
+								className="absolute bg-custom-deepPink h-[4px] w-full z-0"
 								style={{left: `${lowerPercentage}%`, right: `${100 - upperPercentage}%`, width: `${(100 - lowerPercentage - (100 - upperPercentage))}%`}}
 							></div>
 							<div
 								ref={upperRef}
-								className="absolute w-[16px] h-[16px] top-[-4px] rounded-md bg-custom-deepPink cursor-pointer"
-								onMouseDown={handleMouseDownUpper}
-								onMouseUp={handleMouseUpUpper}
-								style={{right: `calc(${100 - upperPercentage}% - 8px)`}}
+								className="absolute w-[16px] h-[16px] top-[-6px] rounded-lg bg-custom-deepPink cursor-pointer z-10"
+								onMouseDown={handleUpperMouseDown}
+								onMouseUp={handleUpperMouseUp}
+								style={{right: `calc(${100 - upperPercentage}% - 16px)`}}
 							>
 							</div>
 						</div>
